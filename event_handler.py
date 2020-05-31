5eb7d788f1e3e819d75368c55de94931b1c5d6d1
William Curry <christian.curry17@gmail.com>
2020-05-31 11:20:35 -0400

    cleaned up logging in event_handler

--- event_handler.py
+++ event_handler.py
@@ -10,21 +10,15 @@ class EventHandler:
         self._use_verbose_logging = use_verbose_logging
 
     def _write_progress_to_console(self, num_processed, total_to_process, process_num_processed_list, sub_lists, process_list):
-        #overall_percentage_str = self.__get_progress_percentage_str(num_processed, total_to_process)
-        output_str = "Overall Progress: " + str(num_processed) + "/" + str(total_to_process) + " " #+ overall_percentage_str + "%" TODO:
+        output_str = "Overall Progress: " + str(num_processed) + "/" + str(total_to_process) + " "
 
         for i in range(0, len(process_num_processed_list)):
-            #percentage_str = self.__get_progress_percentage_str(process_num_processed_list[i], len(sub_lists[i]))
             is_running_str = "Running" if self._is_process_running(process_list[i]) else "Stopped"
-            output_str += ",    P" + str(i+1) + "(" + is_running_str + ")" + ": " + str(process_num_processed_list[i]) + "/" + str(len(sub_lists[i]))  + " " # + percentage_str + "%" TODO
+            output_str += ", P" + str(i+1) + "(" + is_running_str + ")" + ": " + str(process_num_processed_list[i]) + "/" + str(len(sub_lists[i]))  + " "
 
         sys.stdout.write(output_str + "\r")
         sys.stdout.flush()
     
-    def _get_progress_percentage_str(self, progress, total): 
-        percentage = round((float(progress) / float(total)), 3)
-        return str(percentage)
-
     def _some_process_is_alive(self, process_list):
         for process in process_list:
             if process.is_alive():
